186349df0d356a52bdfa84a168180c88ae3aef9e
matheuscosme <mcosme.tavares@gmail.com>
2023-01-26 21:15:42 -0300

    testing axios

--- src/Components/Characters/Characters.jsx
+++ src/Components/Characters/Characters.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Characters.module.css";
+import axios from "axios";
 
 const Characters = () => {
+
+    const [person, setPerson] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/1/')
+        .then((response) => {
+          
+            console.log(response.data)
+            setPerson((response.data));
+         
+        }).catch(() => {
+          
+        })
+
+      }, []);
+
     return (
         <>
         <div className={styles.characters}>
-            <h1>CHARACTERS</h1>
+            <h1>Personagem: {person.name}</h1>
         </div>
         </>
     )
